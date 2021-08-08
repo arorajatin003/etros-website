f62eaf8534a7c8d61729594757ee604c3504a098
Jatin Arora <arorajatin003@gmail.com>
2021-08-09 00:04:52 +0530

    bugs-fixed

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://Jatin-arora-admin:Jatinarora003@cluster0.osicw.mongodb.net/etrosDB", {useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/etros_newDB", {useNewUrlParser: true,useUnifiedTopology: true});
 // mongodb://localhost:27017/
+// mongodb+srv://Jatin-arora-admin:Jatinarora003@cluster0.osicw.mongodb.net/etrosDB
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
